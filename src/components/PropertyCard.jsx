a0ab104e266c73b76d960eed4fc17759049ad7c5
Pratik <srivastavapratik35@gmail.com>
2024-02-22 20:22:35 +0530

    some

--- src/components/PropertyCard.jsx
+++ src/components/PropertyCard.jsx
@@ -1,13 +1,12 @@
-import { IoBanOutline } from "react-icons/io5";
-import { CiStar } from "react-icons/ci";
+import React from "react";
+import Slider from "react-slick";
 import { FaLocationDot } from "react-icons/fa6";
 import PropTypes from "prop-types";
-
-// Import Swiper styles
-import "swiper/css";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const PropertyCard = ({
-  image,
+  images,
   location,
   owner,
   places,
@@ -18,28 +17,44 @@ const PropertyCard = ({
   dimension1,
   dimension2,
   about,
+  number,
   amt,
 }) => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+
   return (
     <div className="relative z-20 flex flex-1 flex-col xl:w-1/2">
+      <Slider {...settings}>
+        {images.map((image, index) => (
+          <div key={index}>
+            <img
+              src={image}
+              alt=""
+              className="rounded-xl h-96 w-50 sm:w-full sm:h-auto"
+            />
+          </div>
+        ))}
+      </Slider>
       <div className="bg-gray-900 mt-10 rounded-xl flex sm:flex-col shadow-pink-500 shadow-lg items-center">
         <div className="">
           <img
-            src={`${image}`}
+            src={`${images[0]}`}
             alt=""
             className="rounded-xl h-96 w-50 sm:w-full sm:h-auto"
           />
         </div>
         <div className="bg-white ml-[-5rem] rounded-xl p-5 h-[90%] sm:w-full sm:ml-0 sm:mt-0 w-[50%] ">
           <div className="flex justify-between">
-            <h1 className="text-lg">{location}</h1>
-            <div className="flex gap-5">
-              <IoBanOutline className="text-[1.5rem]" />
-              <CiStar className="text-[1.5rem]" />
-            </div>
+            <h1 className="text-lg font-bold">{location}</h1>
           </div>
           <div className="">
-            <h1 className="font-bold">{owner}</h1>
+            {/* <h1 className="font-bold">{owner}</h1> */}
             <div className="mt-2 text-blue-500 flex gap-1 items-center">
               <FaLocationDot />
               {places}
@@ -51,7 +66,7 @@ const PropertyCard = ({
                 <h1 className="text-2xl sm:text-lg font-bold">{price1}</h1>
                 <span className="text-gray-50">{amt}</span>
               </div>
-              <h5 className="text-gray-20 text-xs">{price2}</h5>
+              {/* <h5 className="text-gray-20 text-xs">{price2}</h5> */}
             </div>
             <div className="mt-5">
               <div className="flex gap-1 items-baseline">
@@ -62,34 +77,34 @@ const PropertyCard = ({
                 ({area2} sq.m) Super build-up Area
               </h5>
             </div>
-            <div className="mt-5">
+            {/* <div className="mt-5">
               <div className="flex gap-1 items-baseline">
                 <h1 className="text-2xl sm:text-lg font-bold">
                   {dimension1}BHK
                 </h1>
               </div>
               <h5 className="text-gray-20 text-xs">{dimension2} Baths</h5>
-            </div>
+            </div> */}
           </div>
-          <div className="mt-10">{about}</div>
-          <div className="flex gap-2">
+          <div className="mt-10 sm:mt-5">{about}</div>
+          {/* <div className="flex gap-2">
             <div className="bg-pink-300 object-cover rounded-md">
               <h5 className="text-[0.6rem] p-2">Features</h5>
             </div>
             <div className="bg-blue-300 object-cover rounded-md">
               <h1 className="text-[0.6rem] p-2 ">UNDER CONSTRUCTION</h1>
             </div>
-          </div>
+          </div> */}
           <div className="mt-5 flex justify-end gap-5 items-center">
             <a
               href="tel:%2B918207637632"
               target="_blank"
               rel="noopener noreferrer"
             >
-              <h1 className="sm:text-sm">View Phone Number</h1>
+              <h1 className="sm:text-sm">{number}</h1>
             </a>
             <a
-              href="https://wa.me/+918207637632"
+              href={`https://wa.me/+91${number}`}
               target="_blank"
               rel="noopener noreferrer"
             >
@@ -105,7 +120,7 @@ const PropertyCard = ({
 };
 
 PropertyCard.propTypes = {
-  image: PropTypes.string.isRequired,
+  images: PropTypes.arrayOf(PropTypes.string).isRequired,
   location: PropTypes.string.isRequired,
   owner: PropTypes.string.isRequired,
   places: PropTypes.number.isRequired,
--- src/components/Test3.jsx
+++ src/components/Test3.jsx
@@ -2,104 +2,20 @@ import { MdOutlineBathtub } from "react-icons/md";
 import Navbar from "./Navbar";
 import { IoBed } from "react-icons/io5";
 import { SlSizeActual } from "react-icons/sl";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import Footer from "./Footer";
-import { RxCaretDown, RxCaretUp } from "react-icons/rx";
-import DumDum from "./DumDum";
-import Barasat from "./Barasat";
-import NewTown from "./NewTown";
 import Testimonials from "./Testimonials";
 import Contact from "./Contact";
-import Call from "./Call";
 import House from "./House";
-
-const SaleComponent = () => (
-  <div className="bg-white border-t-red-500 rounded-lg p-4 flex flex-wrap items-center gap-10">
-    <div>
-      <span className="text-gray-20">Location</span>
-      <h1 className="text-lg font-bold">Kolkata, West Bengal</h1>
-    </div>
-    <div className="border-l border-gray-20 hidden lg:flex h-10 gap-3"></div>
-    <button className="bg-[#7065F0] text-white p-4 rounded-lg">
-      Browse Properties
-    </button>
-  </div>
-);
-
-const TenentComponent = () => (
-  <div className="bg-white border-t-red-500 rounded-lg p-4 flex flex-wrap items-center gap-10 ">
-    <div>
-      <h1 className="bold-32 sm:items-center sm:text-center lg:bold-32">
-        {" "}
-        We make it easy for <br /> tenants and landlord .
-      </h1>
-      <p className="mt-5 text-gray-30 max-w-sm">
-        Whether you find yourself in the process of selling your current home,
-        seeking financing, or diving into the exciting venture of purchasing a
-        new home, our website is designed to be your ultimate resource, making
-        every step of your real estate journey easy and efficient. What sets us
-        apart is not just our commitment to simplifying complex transactions but
-        also the significant savings of both time and money that you&apos;ll
-        experience by choosing our services.
-      </p>
-      <button className="bg-[#7065F0] px-10 rounded-lg p-4 text-white mt-10">
-        See more
-      </button>
-    </div>
-  </div>
-);
-
-const LandlordComponent = () => (
-  <div className="bg-white border-t-red-500 rounded-lg p-4 flex flex-wrap items-center gap-10">
-    hello
-  </div>
-);
 
 const Test3 = () => {
   useEffect(() => {
     document.title = "Property Platform "; // Set your desired page title here
   }, []);
-  const [activeLink, setActiveLink] = useState("Rent");
-  const [activeeLink, setActiveeLink] = useState("For Tenents");
-
-  const Properties_Links = [
-    { href: "", key: "home", label: "Rent" },
-    { href: "", key: "about", label: "Buy" },
-    { href: "", key: "services", label: "Sell" },
-  ];
-  const Info_Links = [
-    { href: "", key: "home", label: "For Tenents" },
-    { href: "", key: "about", label: "For Landlords" },
-  ];
-
-  const handleLinkClick = (event, label) => {
-    // Prevent the default behavior of the anchor tag
-    event.preventDefault();
-    setActiveLink(label);
-  };
-  const handleeLinkClick = (event, label) => {
-    // Prevent the default behavior of the anchor tag
-    event.preventDefault();
-    setActiveeLink(label);
-  };
-
-  const [isDropdownOpen, setDropdownOpen] = useState(false);
-  const [selectedOption, setSelectedOption] = useState("Option 2");
-
-  const toggleDropdown = () => {
-    setDropdownOpen(!isDropdownOpen);
-  };
-
-  const handleOptionSelect = (option) => {
-    setSelectedOption(option);
-    setDropdownOpen(false);
-    // You can perform additional actions based on the selected option if needed
-  };
 
   return (
     <>
       <Navbar />
-      {/* <Call /> */}
       <div className="bg-[#F7F7FD]">
         <section className="max-container padding-container flex flex-col gap-20 sm:pb-0 py-10 pb-32 md:gap-28 lg:py-20 xl:flex-row">
           <div className="hero-map" />
@@ -113,55 +29,7 @@ const Test3 = () => {
               your dream property – a seamless marketplace for all your real
               estate needs.
             </p>
-            {/* 
-            <div className="flexStart gap-16 sm:items-center">
-              <div className="my-5  flex flex-col ">
-                <div className="bold-32 fkex text-[#7065F0]">
-                  <div className="border-l border-gray-500  h-10 gap-3">
-                    {" "}
-                    <span className="ml-2">50k+</span>
-                  </div>
-                </div>
-                <span className="text-gray-20 ml-2">renters</span>
-              </div>
-              <div className="my-5  flex flex-col ">
-                <div className="bold-32 fkex flex-col text-[#7065F0]">
-                  <div className="border-l border-gray-500  h-10 gap-3">
-                    <span className="ml-2">50k+</span>
-                  </div>
-                </div>
-                <span className="text-gray-20 ml-2">renters</span>
-              </div>
-            </div> */}
 
-            {/* <div className="mt-5">
-              <ul className="flex rounded-lg bg-white px-5 p-2 gap-12">
-                {Properties_Links.map((link) => (
-                  <a
-                    key={link.key}
-                    href={link.href}
-                    onClick={(event) => handleLinkClick(event, link.label)}
-                    className={`text-gray-50 text-xl cursor-pointer pb-1.5 transition-all ${
-                      activeLink === link.label
-                        ? "font-bold border-b-blue-400"
-                        : ""
-                    }`}
-                  >
-                    {link.label}
-                  </a>
-                ))}
-              </ul>
-              {activeLink === "Rent" && <SaleComponent />}
-              {activeLink === "Buy" && <SaleComponent />}
-              {activeLink === "Sell" && <SaleComponent />} */}
-            {/* <Button type="button" title="Download App" variant="btn_green" />
-              <Button
-                type="button"
-                title="How we work?"
-                icon="/play.svg"
-                variant="btn_white_text"
-              /> */}
-            {/* </div> */}
             <button className="bg-[#7065F0] mt-5 text-white p-4 rounded-lg">
               Browse Properties
             </button>
@@ -202,17 +70,6 @@ const Test3 = () => {
           </div>
         </div>
       </div>
-      {/* <section className="max-container padding-container flex flex-col gap-20 py-10 pb-10 md:gap-28 lg:py-20 xl:flex-row">
-        <video
-          src="/vid.mp4"
-          autoPlay={true}
-          muted
-          className="rounded-lg "
-          loop={true}
-        >
-          Your browser does not support the video tag.
-        </video>
-      </section> */}
 
       <section className="max-container padding-container flex flex-col gap-20 py-10 pb-32 md:gap-28 lg:py-20 xl:flex-row">
         <div className="relative z-20 flex flex-1 justify-center p-4 rounded-lg xl:w-1/2 bg-[#F7F7FD] border-2">
@@ -260,79 +117,12 @@ const Test3 = () => {
             className="h-[30rem] w-[40rem] hidden md:w-full xl:w-auto xl:block self-end"
           />
         </div>
-
-        {/* Side image hidden on smaller devices */}
       </section>
 
       <div className="bg-[#F7F7FD]">
-        <section className="max-container  padding-container  gap-20 py-10 pb-32 sm:pb-0 md:gap-28 lg:py-20 xl:flex-row">
-          <div className="flex flex-row  sm:flex-col sm:justify-start justify-between items-center">
-            <div className="text-center sm:text-left">
-              <h1 className="font-bold text-2xl sm:text-3xl lg:text-4xl">
-                Some of our Premium Properties
-              </h1>
-            </div>
-
-            <div className="relative inline-block mt-4 sm:mt-0">
-              <button
-                onClick={toggleDropdown}
-                className="bg-purple-600 px-6 py-3 rounded-lg text-white flex items-center invisible"
-              >
-                Browse more options
-                {isDropdownOpen ? (
-                  <RxCaretDown className="ml-2" />
-                ) : (
-                  <RxCaretUp className="ml-2" />
-                )}
-              </button>
-
-              {isDropdownOpen && (
-                <div className="absolute mt-2 z-50 w-full sm:w-auto bg-white shadow-lg rounded-md py-2 px-4">
-                  {/* Dropdown content goes here */}
-                  <a
-                    className="block text-gray-800 font-bold hover:text-purple-600 py-1 cursor-pointer"
-                    onClick={() => handleOptionSelect("Option 1")}
-                  >
-                    Dum Dum
-                  </a>
-                  <a
-                    className="block text-gray-800 font-bold hover:text-purple-600 py-1 cursor-pointer"
-                    onClick={() => handleOptionSelect("Option 2")}
-                  >
-                    Barasat
-                  </a>
-                  <a
-                    className="block text-gray-800 font-bold hover:text-purple-600 py-1 cursor-pointer"
-                    onClick={() => handleOptionSelect("Option 3")}
-                  >
-                    New Town
-                  </a>
-                </div>
-              )}
-            </div>
-
-            {/* Render content based on the selected option */}
-          </div>
-
-          {selectedOption === "Option 1" && <DumDum />}
-          {selectedOption === "Option 2" && <House />}
-          {selectedOption === "Option 3" && <NewTown />}
-          {/* <a href="/Bunglow">
-            <button className="bg-[#7065F0] px-10 rounded-lg p-4 text-white mt-10">
-              See more
-            </button>
-          </a> */}
+        <section className="py-5 ">
+          <House />
         </section>
-        {/* <section className="max-container  padding-container  gap-20 py-10 pb-32 md:gap-28 lg:py-20 xl:flex-row">
-          <div className="text-center">
-            <h1 className="bold-32 sm:items-center sm:text-center lg:bold-52 text-center">
-              Testimonials
-            </h1>
-            <p className="text-gray-20">
-              See what our property manages, landlords, and tenents have to say
-            </p>
-          </div>
-        </section> */}
       </div>
       <Testimonials />
       <div id="contact">
